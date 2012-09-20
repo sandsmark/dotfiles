c456e71987b0f04497fcb3911bcb2a064338b652
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2012-09-20 20:21:01 +0200

    add muttrc

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 
-for file in vimrc bashrc gitconfig gitexcludes; do
+for file in vimrc bashrc gitconfig gitexcludes muttrc; do
     rm -f ~/.$file
     ln -s $PWD/$file ~/.$file;
 done
