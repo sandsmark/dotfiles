601718bca1ddb4995be5e75cc8343b4f2ce61e56
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2018-10-18 10:02:23 +0200

    add offlineimaprc

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -xe
 
-for file in vimrc bashrc gitconfig gitexcludes muttrc bcrc lsan.suppressions; do
+for file in vimrc bashrc gitconfig gitexcludes muttrc bcrc lsan.suppressions offlineimaprc; do
     rm -f ~/.$file
     ln -s $PWD/$file ~/.$file;
 done
